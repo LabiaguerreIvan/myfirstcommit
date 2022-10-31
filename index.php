8ab1981d39ebb84232ddbd556cf9aa8d7d75b1c1
Ivan Labiaguerre <labiaguerreivan@gmail.com>
2022-10-31 11:47:02 -0300

    colegio31/10

--- index.php
+++ index.php
@@ -1,1 +1,232 @@
-fdafdnfkpfoaknmfp´ffp´fldf
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/product/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+        }
+    }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="product.css" rel="stylesheet">
+</head>
+<body>
+    <div id="bg">
+    <header class="site-header sticky-top py-1">
+    <nav class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="" aria-label="Inicio">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Inicio</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+            <a class="py-2 d-none d-md-inline-block" href="#nosotros">Nosotros</a>
+            <a class="py-2 d-none d-md-inline-block" href="">Sucursales</a>
+            <a class="py-2 d-none d-md-inline-block" href="">Catálogo</a>
+            <a class="py-2 d-none d-md-inline-block" href="#contactanos">Contactanos</a>
+            <a class="py-2 d-none d-md-inline-block" href="">Iniciar Sesion</a>
+    </nav>
+</header>
+
+<main>
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <h1 class="display-4 fw-normal"><i>Ferreteria "Labeitas"</i></h1>
+    <br>
+    <br>
+    <p>
+    <a class="btn btn-outline-secondary" href="#">Electronica</a>
+    &nbsp;&nbsp;
+    <a class="btn btn-outline-secondary" href="#">Cables</a>
+    &nbsp;&nbsp;
+    <a class="btn btn-outline-secondary" href="#">Herramientas</a>
+</p>
+<p>
+    <a class="btn btn-outline-secondary" href="#">IIluminacion</a>
+    &nbsp;&nbsp;
+    <a class="btn btn-outline-secondary" href="#">Otros Productos</a>
+    </p>    
+</div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
+
+<!-- fila 1 - card 1 -->
+<div class="container text-center">
+    <div class="row">
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 1 - card 2 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 1 - card 3 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+</div>   
+
+<p></p>
+
+<!-- fila 2 - card 1 -->
+<div class="container text-center">
+    <div class="row">
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 2 - card 2 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 2 - card 3 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+</div>
+
+<p></p>
+
+<!-- fila 3 - card 1 -->
+<div class="container text-center">
+    <div class="row">
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 3 - card 2 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+
+    <!-- fila 3 - card 3 -->
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+    </div>
+    </div>
+</div>    
+</div>
+</main>
+
+<footer class="container py-5">
+<div class="row">
+    <div class="col-12 col-md">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+    <small class="d-block mb-3 text-muted">&copy; </small>
+    </div>
+    
+    
+        <div class="col-6 col-md">
+    <h5>Nosotros</h5>
+    <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="#">Team</a></li>
+        <li><a class="link-secondary" href="#">Locations</a></li>
+        <li><a class="link-secondary" href="#">Privacy</a></li>
+        <li><a class="link-secondary" href="#">Terms</a></li>
+    </ul>
+    </div>
+
+    <div class="col-6 col-md">
+    <h5>Contactanos</h5>
+    <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="#">Resource name</a></li>
+        <li><a class="link-secondary" href="#">Resource</a></li>
+        <li><a class="link-secondary" href="#">Another resource</a></li>
+        <li><a class="link-secondary" href="#">Final resource</a></li>
+    </ul>
+    </div>
+</div>
+</footer>
+</div>
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+</body>
+</html>
