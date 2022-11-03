90f7799d8045ff2e938fb6d3a3c2048b2bd7e10a
Ivan Labiaguerre <labiaguerreivan@gmail.com>
2022-11-03 13:15:09 -0300

    3/11/2022

--- index.php
+++ index.php
@@ -57,20 +57,16 @@
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
     <h1 class="display-4 fw-normal"><i>Ferreteria "Labeitas"</i></h1>
-    <br>
-    <br>
-    <p>
+    &nbsp;
     <a class="btn btn-outline-secondary" href="#">Electronica</a>
     &nbsp;&nbsp;
     <a class="btn btn-outline-secondary" href="#">Cables</a>
     &nbsp;&nbsp;
     <a class="btn btn-outline-secondary" href="#">Herramientas</a>
-</p>
-<p>
+    &nbsp;&nbsp;
     <a class="btn btn-outline-secondary" href="#">IIluminacion</a>
     &nbsp;&nbsp;
-    <a class="btn btn-outline-secondary" href="#">Otros Productos</a>
-    </p>    
+    <a class="btn btn-outline-secondary" href="#">Otros Productos</a>   
 </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
